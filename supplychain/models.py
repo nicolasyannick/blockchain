d1c348fe8bdc474f757981582b90096bacf4cb45
Yannick <nicolasyannick3510@gmail.com>
2023-11-10 22:52:53 +0400

    supplychain models

---
+++ supplychain/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=75)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    order_date = models.DateTimeField(auto_now_add=True)
+    STATUS_CHOICES = [
+        ('ordered', 'Ordered'),
+        ('shipped', 'Shipped'),
+        ('delivered', 'Delivered'),
+    ]
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='ordered')
+
+    def __str__(self):
+        return f"Order of {self.product.name} on {self.order_date}"
+
